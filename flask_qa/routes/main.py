1668c67aa10950f0af9c199ed197443ccc8631e3
Josh <josh.vansant99@gmail.com>
2019-12-09 14:54:59 -0600

    added postgresql

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -7,7 +7,31 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    user = Player(
+        id =0,
+        name = "bob",
+        team = "lakers",
+        gp = 1,
+        gs = 2,
+        min =3,
+        pts = 4,
+        ro = 5,
+        dr = 6,
+        reb = 7,
+        ast = 8,
+        stl = 9,
+        blk = 10,
+        to = 11,
+        pf = 12,
+        astTo = 13,
+        per = 14
+        )
+    db.session.add(user)
+    db.session.commit()
     asdf=Player.query.all()
     return render_template('result.html', result=asdf)
+
+
+
 
 # user = User.query.filter_by(name=name).first()
